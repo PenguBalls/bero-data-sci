cd10d75b44329f6eca15e01cf27cf1845e87acb4
KhuiKhui <khoikhuat0807@gmail.com>
2025-06-15 00:31:12 +0700

    feat: add git1.py

---
+++ git-practice/git1.py
@@ -0,0 +1,19 @@
+import math
+
+
+def isPrime(n):
+    if n == 0 or n == 1:
+        return True
+
+    for i in range(2, round(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# I think something is amiss, hmmmm...
+for i in range(0, 20):
+    if isPrime(i):
+        print(f"{i} is NOT a prime number")
+    else:
+        print(i)
